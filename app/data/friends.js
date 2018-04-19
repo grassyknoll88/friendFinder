5c12a77c7238c4fa9eedf812b9934db3f51e65c8
Ben Knoll <bknoll66@gmail.com>
2018-04-19 18:10:13 -0500

    changes to apiRoutes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,47 +1,21 @@
 console.log("connected");
 
 var people = [
-    {
-        name: "Kiley",
-         photo: "https://photos.google.com/share/AF1QipOhWr598AKTcxBJg8LIbFQjo6jLWTonLy67qZtljdse0dfRdropA4R2bCqhZBAX3Q?key=XzRLRGhwM3YzMnNidXNJeGdRdWpCMVNZczFHamdB",
-         scores: ["3", "3", "3", "3", "3", "1", "2", "3", "3", "4"]
-    },
-    {
-        name: "Hannah",
-         photo: "https://photos.app.goo.gl/gr7gBJh8Cxmk7viO2",
-         scores: ["4", "3", "1", "4", "1", "3", "1", "5", "2", "4"]
-    },
-    {
-        name: "Beau",
-         photo: "https://photos.google.com/share/AF1QipNsUMPT4FAJUegYefIRSdezAERI_fTX4hDBU42PHJ5aBLstzoVjEUrHcZUqtkPsUQ?key=a0RwYmZRV3hWVzhkWjV0aE5KemQ2bVN6T1R4aDlB",
-         scores: ["5", "3", "4", "2", "4", "3", "2", "3", "2", "4"]
-    }
-]
-
-$("#add-btn").on("click", function(event) {
-    event.preventDefault();
-    var newFriend = {
-      name: $("#name").val(),
-      photo: $("#photo").val(),
-      scores: [
-        $("#q1").val(),
-        $("#q2").val(),
-        $("#q3").val(),
-        $("#q4").val(),
-        $("#q5").val(),
-        $("#q6").val(),
-        $("#q7").val(),
-        $("#q8").val(),
-        $("#q9").val(),
-        $("#q10").val()
-      ]
-    };
-
-    console.log(newFriend);
-      
-
-    $.post("/api/new", newFriend)
-      .then(function(data) {
-        console.log(data);
-      });
-  });
+  {
+    name: "Kiley",
+    photo:
+      "https://photos.google.com/share/AF1QipOhWr598AKTcxBJg8LIbFQjo6jLWTonLy67qZtljdse0dfRdropA4R2bCqhZBAX3Q?key=XzRLRGhwM3YzMnNidXNJeGdRdWpCMVNZczFHamdB",
+    scores: ["3", "3", "3", "3", "3", "1", "2", "3", "3", "4"]
+  },
+  {
+    name: "Hannah",
+    photo: "https://photos.app.goo.gl/gr7gBJh8Cxmk7viO2",
+    scores: ["4", "3", "1", "4", "1", "3", "1", "5", "2", "4"]
+  },
+  {
+    name: "Beau",
+    photo:
+      "https://photos.google.com/share/AF1QipNsUMPT4FAJUegYefIRSdezAERI_fTX4hDBU42PHJ5aBLstzoVjEUrHcZUqtkPsUQ?key=a0RwYmZRV3hWVzhkWjV0aE5KemQ2bVN6T1R4aDlB",
+    scores: ["5", "3", "4", "2", "4", "3", "2", "3", "2", "4"]
+  }
+];
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+require("./routing/apiRoutes");
+require("./routing/htmlRoutes");
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
