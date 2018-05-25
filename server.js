a81bb542517788899eb71da6579faf16dd363306
Ben Knoll <bknoll66@gmail.com>
2018-05-24 20:09:28 -0500

    route update

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+app.use(express.static("app/public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
